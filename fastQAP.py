24c3eb2d4165e087a5bc3e93845dfb3489fe19f2
sidekicklx <lixuezixiang@gmail.com>
2018-06-01 14:22:37 -0700

    Add String Edit distance

--- fastQAP.py
+++ fastQAP.py
@@ -1,40 +1,55 @@
-# -*- coding:utf-8 -*- 
+# -*- coding:utf-8 -*-
 
-import sys, getopt
+import sys
+import getopt
 import numpy
 from scipy import optimize
 
-def normolize_mat(M) :
+
+def normolize_mat(M):
     mmin, mmax = M.min(), M.max()
     return (M-mmin)/(mmax-mmin)
-def fun(alpha, A, B, P, Q) :
+
+
+def fun(alpha, A, B, P, Q):
     P_m = P + alpha*Q
     return -numpy.trace(A @ P_m @ B.T @ P_m.T)
 
-def grad(A, B, P) :
-    return  -(A @ P @ B.T) - (A.T @ P @ B)
 
-def hungarian_alg(grad_P) :
-    #The method used is the Hungarian algorithm, also known as the Munkres or Kuhn-Munkres algorithm.
-    assign_idx, assign_vec = optimize.linear_sum_assignment(grad_P) 
+def grad(A, B, P):
+    return -(A @ P @ B.T) - (A.T @ P @ B)
+
+
+def hungarian_alg(grad_P):
+    # The method used is the Hungarian algorithm,
+    # also known as the Munkres or Kuhn-Munkres algorithm.
+    print(grad_P)
+    print("=========================")
+    assign_idx, assign_vec = optimize.linear_sum_assignment(grad_P)
     Q_i_T = numpy.zeros(grad_P.shape)
     for row in assign_idx:
         Q_i_T[row, assign_vec[row]] = 1
     return Q_i_T.T
 
-def min_arg(A, B, P, Q) :
+
+def min_arg(A, B, P, Q):
     alpha = optimize.fminbound(fun, 0, 1, [A, B, P, Q])
     return alpha
+
 
 def get_step(P_next, P):
     return numpy.linalg.norm(P_next-P, 'fro')
 
-def permu_projector(P) :
-    #Maximum is the same as minimum problem
-    #We find the maximum element m in the matrix P
-    #then let new P = m * [1] - P
-    #So far the problem has turned into a minimum assign problem, use the same method above.
-    N= P.shape
+
+def permu_projector(P):
+    '''
+    Maximum is the same as minimum problem
+    We find the maximum element m in the matrix P
+    then let new P = m * [1] - P
+    So far the problem has turned into a minimum assign problem,
+    use the same method above.
+    '''
+    N = P.shape
     P_reverse = numpy.max(P) * numpy.ones(N) - P
     assign_idx, assign_vec = optimize.linear_sum_assignment(P_reverse)
     P_f = numpy.zeros(N)
@@ -42,34 +57,36 @@ def permu_projector(P) :
         P_f[row, assign_vec[row]] = 1
     return P_f
 
-def get_QAP_score(A, B, P) :
+
+def get_QAP_score(A, B, P):
     gap = A - P @ B @ P.T
     return numpy.linalg.norm(gap, 'fro')
 
-def fast_qap(A, B, IMAX, sigma) :
-    #A, B should be n*n matrix.
+
+def fast_qap(A, B, IMAX, sigma):
+    # A, B should be n*n matrix.
     N = A.shape
     P = numpy.ones(N)/N[1]
     i = 0
     step = sigma
-    #normolize A & B
+    # normolize A & B
     A_norm = normolize_mat(A)
     B_norm = normolize_mat(B)
-    while (i <  IMAX) and (step >= sigma) :
+    while (i < IMAX) and (step >= sigma):
         grad_P = grad(A_norm, B_norm, P)
         Q_i = hungarian_alg(grad_P.T)
         P_next = P + min_arg(A_norm, B_norm, P, Q_i) * Q_i
         step = get_step(P_next, P)
         P = P_next.copy()
         i = i + 1
-    result_P  = permu_projector(P)
+    result_P = permu_projector(P)
     return get_QAP_score(A_norm, B_norm, result_P)
 
 
-def  main(argv=None) :
+def main(argv=None):
     inputfile = ""
     try:
-        opts, args = getopt.getopt(argv, "i:",["infile="])
+        opts, args = getopt.getopt(argv, "i:", ["infile="])
     except getopt.GetoptError:
         print("Error: test_arg.py -i <inputfile>")
         return 2
@@ -77,7 +94,7 @@ def  main(argv=None) :
         if opt in ("-i", "--infile"):
             inputfile = arg
     print("Parsing file", inputfile, "...")
-    
+
     A = numpy.array([[1, 1, 0, 0, 1, 0, 0, 0, 1, 0],
                      [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -99,12 +116,13 @@ def  main(argv=None) :
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 1, 1, 0, 0, 1, 0, 0]])
-                       
-    #C = numpy.random.random((10,10))
-   
+
+    # C = numpy.random.random((10,10))
+
     print("score:", fast_qap(A, B, 30, 1.0e-7))
-    #print(fast_qap(A, C, 50, 1.0e-7))
+    # print(fast_qap(A, C, 50, 1.0e-7))
     return 0
 
+
 if __name__ == "__main__":
-   sys.exit(main(sys.argv[1:]))
+    sys.exit(main(sys.argv[1:]))
--- graphAnalyzer/graphAna.py
+++ graphAnalyzer/graphAna.py
@@ -1,36 +1,63 @@
-import sys, getopt
+import sys
 import pydot
-import pyparsing
 import numpy
+import re
 
 
-def  main(argv=None) :
-    inputfile = ""
-    try:
-        opts, args = getopt.getopt(argv, "i:",["infile="])
-    except getopt.GetoptError:
-        print("Error: graphAna.py -i <inputfile>")
-        print("\tor: graphAna.py --infile=<inputfile>")
-        return 2
-    for opt, arg in opts:
-        if opt in ("-i", "--infile"):
-            inputfile = arg
-    graphlst = pydot.graph_from_dot_file(inputfile)
+# Levenshtein distance
+def levenshteinDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(
+                    1 + min((distances[i1], distances[i1 + 1],
+                             distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
+
+def dot_file_parser(filename):
+    graphlst = pydot.graph_from_dot_file(filename)
     graph = graphlst[0]
     nodes = graph.get_nodes()
     edges = graph.get_edges()
     adj_matrix = numpy.zeros((len(nodes), len(nodes)))
     nodelist = []
+    node_stmt_list = []
     for node in nodes:
+        node_stmt_list.append(
+            re.search('{.*}', node.to_string()[12:-1]).group())
         nodelist.append(node.get_name())
-    for edge in edges :
-        adj_matrix[nodelist.index(edge.get_source()[:12]), nodelist.index(edge.get_destination()[:12])] = 1
-        adj_matrix[nodelist.index(edge.get_destination()[:12]), nodelist.index(edge.get_source()[:12])] = 1
-    print(adj_matrix)
+    for edge in edges:
+        adj_matrix[nodelist.index(edge.get_source()[:12]), nodelist.index(
+            edge.get_destination()[:12])] = 1
+    return adj_matrix, node_stmt_list
+
+
+def main(argv=None):
+    inputfile1 = ("C:\\Users\\lixue\\Project\\FastQAP\\fastQAPy\\graphAnalyzer"
+                  "\\cfg.fib.dot")
+    inputfile2 = ("C:\\Users\\lixue\\Project\\FastQAP\\fastQAPy\\graphAnalyzer"
+                  "\\cfg.fib.dot")
+    adj_matrix_A, node_stmt_list_A = dot_file_parser(inputfile1)
+    adj_matrix_B, node_stmt_list_B = dot_file_parser(inputfile2)
+    diff_map = numpy.zeros((len(node_stmt_list_A), len(node_stmt_list_B)))
+    for i_A, node_A in enumerate(node_stmt_list_A):
+        for i_B, node_B in enumerate(node_stmt_list_B):
+            diff = levenshteinDistance(node_A, node_B)
+            if(diff < len(node_A) and diff < len(node_B)):
+                diff_map[i_A][i_B] = diff
+            else:
+                diff_map[i_A][i_B] = float("inf")
+    print(diff_map)
+
 
 if __name__ == "__main__":
-   sys.exit(main(sys.argv[1:]))
-
-
-
-
+    sys.exit(main(sys.argv[1:]))
